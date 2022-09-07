107a65ac3afe5a81d78cf8a027c0a2bb63dc39fb
Igor Rocha <igor_roc@hotmail.com.br>
2022-09-07 12:20:45 -0300

    [add]: Programacao object

---
+++ src/utils/Programacao.js
@@ -0,0 +1,64 @@
+export default {
+	info: [
+		{
+			dia: "Quarta-feira",
+			programacao: [
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Abertura",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao: "Apresentação do evento e palestra de abertura.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+				},
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Abertura",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao:
+						"Labore in culpa non eu minim voluptate dolor cupidatat cillum incididunt nulla cupidatat. Anim sunt fugiat fugiat eu. Ipsum cillum eiusmod id aliquip non est cillum Lorem fugiat excepteur officia ipsum occaecat. Pariatur laboris ut amet qui elit mollit labore amet ut sint. Non pariatur nisi in nulla laborum do eu in amet reprehenderit magna est velit voluptate.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+				},
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Minicurso Blockchain",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao: "Apresentação do evento e palestra de abertura.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+					tipo: "minicurso",
+				},
+			],
+		},
+		{
+			dia: "Quinta-feira",
+			programacao: [
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Abertura",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao: "Apresentação do evento e palestra de abertura.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+				},
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Abertura",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao: "Apresentação do evento e palestra de abertura.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+				},
+				{
+					inicio: "19:00",
+					fim: "19:30",
+					titulo: "Abertura",
+					palestrante: "Prof. Dr. José Carlos de Souza",
+					descricao: "Apresentação do evento e palestra de abertura.",
+					link: "https://www.youtube.com/watch?v=1Q8fG0TtGRQ",
+				},
+			],
+		},
+	],
+}
