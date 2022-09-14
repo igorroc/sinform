648a9ff7794c79c30d9b8ec4ec51df7fafdb9e17
igorroc <igor_roc@hotmail.com.br>
2022-09-14 12:42:40 -0300

    [add]: Allow dist folder to commit

---
+++ dist/Palestrantes.js
@@ -0,0 +1,86 @@
+import img from "../src/assets/palestrantes/placeholder.jpg"
+import imgIgor from "../src/assets/palestrantes/igorrocha.jpg"
+import imgElinaldo from "../src/assets/palestrantes/elinaldo.jpg"
+import imgPaula from "../src/assets/palestrantes/paula.png"
+import imgIngrid from "../src/assets/palestrantes/ingrid.png"
+import imgHenrique from "../src/assets/palestrantes/henrique.png"
+import imgFlavio from "../src/assets/palestrantes/flavio.png"
+import imgKatia from "../src/assets/palestrantes/katia.png"
+import imgAllan from "../src/assets/palestrantes/allan.png"
+import imgThiago from "../src/assets/palestrantes/thiago.png"
+
+export default {
+	info: [
+		{
+			nome: "Ingrid Winkler",
+			id: "IngridWinkler",
+			foto: imgIngrid,
+			descricao:
+				"Lidera o Grupo de Pesquisa CNPQ 'Realidade Virtual, Realidade Aumentada e mundos virtuais para a inovação na Indústria'",
+			variacao: "extra leaf BR green",
+		},
+		{
+			nome: "Paula Dornhofer Paro Costa",
+			id: "PaulaDornhofer",
+			foto: imgPaula,
+			descricao:
+				"Professora do Departamento de Engenharia de Computação e Automação",
+			variacao: "extra leaf BL green",
+		},
+		{
+			nome: "Flavio Shigeo Yamamoto",
+			id: "FlavioShigeo",
+			foto: imgFlavio,
+			descricao:
+				"Mestre em Ciência da Computação pela USP. Especialista em inteligencia artificial.",
+			variacao: "extra leaf BL green",
+		},
+		{
+			nome: "Katia Vaskys",
+			id: "KatiaVaskys",
+			foto: imgKatia,
+			descricao:
+				"Primeira executiva à frente da IBM Brasil. Especialização em tecnologia e negócios.",
+			variacao: "extra leaf BR green",
+		},
+		{
+			nome: "Allan Edgard Silva Freitas",
+			id: "AllanEdgard",
+			foto: imgAllan,
+			variacao: "extra leaf BR green",
+		},
+		{
+			nome: "Thiago Barros Murari",
+			id: "ThiagoMurari",
+			foto: imgThiago,
+			variacao: "extra leaf BL green",
+		},
+		{
+			nome: "Elinaldo Júnior",
+			id: "ElinaldoJunior",
+			foto: imgElinaldo,
+			descricao: "Professor de Ciência da Computação (UESC)",
+			variacao: "extra dots BL orange",
+		},
+		{
+			nome: "Igor Rocha",
+			id: "IgorRocha",
+			foto: imgIgor,
+			descricao: "Full Stack e Diretor de Projetos da TecnoJr (UESC)",
+			variacao: "extra dots TR orange",
+		},
+		{
+			nome: "Napoleão Batista Lemos Filho",
+			id: "NapoleaoBatista",
+			foto: img,
+			variacao: "extra dots TL orange",
+		},
+		{
+			nome: "Henrique Serra",
+			id: "HenriqueSerra",
+			foto: imgHenrique,
+			descricao: "Full Stack e Desenvolvedor Dapps (UESC)",
+			variacao: "extra dots BR orange",
+		},
+	],
+}
